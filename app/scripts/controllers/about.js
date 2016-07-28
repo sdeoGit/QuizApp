f89efbd325bf1e3b57b4f3b768edb08996fa7e50
Siddarth deo <Siddarth_deo@trimble.com>
2016-07-28 23:42:12 +0530

    #Quiz Initial App folder

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appAngularGruntApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appAngularGruntApp
+ */
+angular.module('appAngularGruntApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appAngularGruntApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appAngularGruntApp
+ */
+angular.module('appAngularGruntApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
